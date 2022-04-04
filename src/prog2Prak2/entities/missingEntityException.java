4a1cead257b382204d13fa8c0d09648ad8aa0427
MiTROSKA <MiTROSKA@DESKTOP-GL5KLFL>
2022-04-04 11:47:11 +0200

    exception added

---
+++ src/prog2Prak2/entities/missingEntityException.java
@@ -0,0 +1,11 @@
+package prog2Prak2.entities;
+
+public class missingEntityException extends RuntimeException{
+	public missingEntityException() {
+		super();
+	}
+	
+	public missingEntityException(String message) {
+		super(message);
+	}
+}
